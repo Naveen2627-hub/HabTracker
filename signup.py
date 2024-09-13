11d56a9f46998dc0ea82ec8caec664224be09d44
Naveen Pasala <pasala.n@northeastern.edu>
2024-09-13 15:37:55 -0500

    Initial Strruture

---
+++ signup.py
@@ -0,0 +1,10 @@
+def validate_email(self,email_id):
+    pass
+
+
+def signup(self):
+    first_name = self.first_name
+    last_name = self.last_name
+    email_id = self.email_id
+    phone_number = self.phone_number
+
